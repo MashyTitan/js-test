99e7f413a7bf6e06b72c76bf35b21c8999ed551a
Keshav Sharma <145584650+MashyTitan@users.noreply.github.com>
2023-12-27 06:44:57 +0000

    fawfwaf

--- mashy/conversion_operations.js
+++ mashy/conversion_operations.js
@@ -6,12 +6,12 @@ let score = "33"
 
 
 
-console.log(typeof score);
-console.log(typeof (score));
+// console.log(typeof score);
+// console.log(typeof (score));
 
-let valueInNum = Number(score)
-console.log(typeof valueInNum);
-console.log(valueInNum); //prints NaN
+// let valueInNum = Number(score)
+// console.log(typeof valueInNum);
+// console.log(valueInNum); //prints NaN
 
 /*
 
@@ -22,11 +22,56 @@ false --> 0
 
 */
 
-let isLoggedIn = 1
-let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// let isLoggedIn = 1
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
 
-let someNumber = 33
-let str_num = String(someNumber);
-console.log(str_num);
-console.log(typeof str_num);
+// let someNumber = 33
+// let str_num = String(someNumber);
+// console.log(str_num);
+// console.log(typeof str_num);
+
+//Operations xd
+// let value=3
+// let negVal = -value
+// console.log(negVal);
+
+// let str1 = "hello"
+// let str2 = "world"
+// str3=str1+str2
+// console.log(str3);
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+"2");
+// console.log("1"+1+3);//agar string pehle hai toh sabko hi string consider karega
+// console.log(1+2+"2");
+// //agar string last me hai toh pehle wale jo hai vahi consider karega
+
+
+//some more conversions but not practiced at all in work
+// console.log(+true);
+// console.log(+"");
+
+
+//prefix and postfix
+// counter=100
+// counter++; 
+// console.log(counter);
+
+// console.log("2">1);
+// console.log("02">1);
+
+// console.log(null>0);
+// console.log(null==0);
+// console.log(null>=0);
+
+console.log(undefined>0);
+console.log(undefined==0);
+console.log(undefined>=0);
+console.log(undefined<=0);
+
+
+
+
+
