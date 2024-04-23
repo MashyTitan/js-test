5ecdea14f4962579e2445346d5b737aa41441d23
Keshav Sharma <145584650+MashyTitan@users.noreply.github.com>
2024-04-23 18:13:04 +0000

    xd

---
+++ test.js
@@ -0,0 +1,13 @@
+// let str1="software developer"
+// console.log(str1)
+// let str2=str1.split(" ")
+// // console.log(str2)
+// str1 = str2[1]+ " " +str2[0]
+// console.log(str1)
+
+
+// const num=34.56777
+// console.log(num.toPrecision(2))
+console.log(Math.random)
+console.log(Math.random() * 10 + 1)
+console.log(Math.floor(Math.random() * 10) + 1)
